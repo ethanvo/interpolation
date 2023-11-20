be1c6d233a89e3a6cbc2740b89a25d2e7c28b10f
Ethan Vo <ethan.vo@hotmail.com>
2023-11-20 12:26:44 -0500

    some mistakes

--- 01_get_bands/makeinput.py
+++ 01_get_bands/makeinput.py
@@ -5,7 +5,7 @@ materials = ["c", "lih"]
 with open('runall.sh', 'w') as f:
     f.write('#!/bin/zsh\n')
     for material in materials:
-        for scfi in range(1, 6):
-            for mpi in range(5, 6):
+        for scfi in range(2, 11):
+            for mpi in range(2, 11):
                 key = '{}_ccpvdz_scf_{}_mp2_{}'.format(material, scfi, mpi)
                 f.write('python3 {}_df_dft_mp2.py {} {} {}.json 1> {}.out 2> {}.err\n'.format(material, scfi, mpi, key, key, key))
